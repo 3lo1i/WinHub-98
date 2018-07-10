bcca96d476de439505cb214227bee99f54580088
3lo1i <38400129+3lo1i@users.noreply.github.com>
2018-07-10 20:58:57 +0300

    Automatic images to base64 conversion

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const cssBase64 = require('gulp-css-base64');
 
 
 gulp.task('build', () => {
   return gulp.src(['./scss/userstyle.scss'])
     .pipe(sass().on('error', sass.logError))
+    .pipe(cssBase64({ baseDir: './img', extensionsAllowed: ['.png'] }))
     .pipe(rename({
       'basename': 'WinHub-98',
       'suffix': '.user'
